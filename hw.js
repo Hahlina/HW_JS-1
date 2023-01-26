de6b8bb49cbc8bef90796a3a0687e3387cb233af
Hahlina <hahlina@e-macis-MacBook-Pro.local>
2023-01-26 21:22:22 +0200

    first commit

---
+++ hw.js
@@ -0,0 +1,81 @@
+"use strict";
+
+//! Task 1
+//  let name1 = prompt("Enter your name: ");
+//   alert (`Your name is: ${name1}`); //коли називаю зміну просто name тут мені перекреслює це слово, хоча код працює, тому просто добавляю цифру)
+
+  //! Task 2
+//   const year = 2023;
+//   let age = +prompt("Enter your year of brith: ");
+//   let result = year - age;
+//   alert (`Are you ${result} years old `);
+
+  //! Task 3
+//   let lenght = +prompt ("Enter the lenght of the side of the square: ");
+//   let perimeter = lenght * 4;
+//   alert (`The perimeter of the square is: ${perimeter}`);
+
+  //! Task 4
+//   const p = 3.14;
+//   let radius = +prompt ("Enter the radius of the circle: ");
+//   let area = p * radius**2;
+//   alert (`The area of the circle is equal to: ${area}`);
+
+  //! Task 5
+//   let distance = +prompt ("Enter the distance between the city (km): ");
+//   let time = +prompt ("Enter the time you want to reach your destination (hr): ");
+//   let speed = distance / time;
+//   alert (`You need to move at a speed of: ${speed} km`);
+
+  //! Task 6
+
+//   const eur = 1.091;
+//   let usd = +prompt (`Enter the amount of dollars to exchange: `);
+//   let exchange = usd / eur;
+//   alert (`You will receive ${exchange} EUR`);
+
+  //! Task 7
+//   const sizeFile = 820;
+//   const gb = 1024; 
+//   let memoryFlash = +prompt ("Enter the volume of the drive in GB: ");
+//   let mb = memoryFlash * gb;
+//   let numberFiles = mb / sizeFile;
+//   alert (`The number of files that can fit on the drive is equal: ${numberFiles}`);
+//  //? Або так)
+//     let memoryFlash2 = +prompt ("Enter the volume of the drive in GB: ");
+//     let result = (memoryFlash2 * 1024) / 820;
+//     alert (`The number of files that can fit on the drive is equal: ${result}`);
+
+  //! Task 8
+//   let money = +prompt ("How much money do you have? ");
+//   let chocolate = +prompt ("Enter the cost of the chocolate bar: ");
+//   let rest = money % chocolate;
+//   let number = (money - rest) / chocolate;
+//   alert (`You can buy - ${number} chocolates and you will have the rest - ${rest} c.u.`);
+
+  //! Task 9
+//   let number = +prompt ("Enter a three-digit number");
+//   if (number >=100 && number <1000){
+//   let action1 = Math.floor((number % 1000) / 100); 
+//   let action2 = Math.floor((number % 100) / 10); 
+//   let action3 = Math.floor((number % 10) / 1); 
+//   let action4 = action3 * 1000 / 10; 
+//   let action5 = action2 * 1000 / 100; 
+//   let action6 = action1 * 1000 / 1000; 
+//   let action7 = action4 + action5 + action6;
+//   alert (`Mirror number is ${action7}`);
+// }
+// else{
+//       console.log("Please enter correct date!");
+// }
+
+  //! Task 10
+//   let number = +prompt ("Enter number");
+//   let rest = number % 2;
+//   let numberCheck = rest ===0 ? true : false;
+//   alert (numberCheck ? "The number is even" : "The number is odd");
+
+
+ 
+
+
